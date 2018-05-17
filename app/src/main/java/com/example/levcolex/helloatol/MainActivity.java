274b1e53b9f30bfb9d64024c06e50ea09687c701
Alex <levcolex@yandex.ru>
2018-05-17 14:11:22 +0300

    try to build first time

--- app/src/main/java/com/example/levcolex/helloatol/MainActivity.java
+++ app/src/main/java/com/example/levcolex/helloatol/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.levcolex.helloatol;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
 
+        import com.atol.drivers.fptr.Fptr;
+        import com.atol.drivers.fptr.IFptr;
+        import com.atol.drivers.fptr.settings.SettingsActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    IFptr fptr = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            fptr = new Fptr();
+            fptr.create(this);
+        } catch (NullPointerException ex){
+            fptr = null;
+        }
+
     }
 }
